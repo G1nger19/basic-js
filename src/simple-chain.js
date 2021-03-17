1e608e358ad270cd4bf6f06c0fec1ed44215e3ad
Nikolas <erosenkov2017@gmail.com>
2021-03-17 22:26:07 +0300

    feat: add solution for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  Chain : [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.Chain.length;
+    return this;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '( )') {
+    if (typeof value === 'undefined') { value = '( )'; }
+    else { value = '( ' + value + ' )'; }
+    this.Chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position === 'number' && (position < this.Chain.length) && (position > 0)) {
+      this.Chain.splice(position-1, 1);
+      return this;
+    }
+    else {
+      this.Chain=[];
+      throw new UserError("the position argument is invalid");      
+    };
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+     this.Chain.reverse();   
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let a = this.Chain.join('~~');
+    this.Chain=[];   
+    return a;
   }
 };
 
