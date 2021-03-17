37b5c8e4b975cc74cfc8897fb567d8da2a679a85
Nikolas <erosenkov2017@gmail.com>
2021-03-17 21:32:48 +0300

    feat: add 2 solutions for extend-repetead

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+ // const addition_str = (options['addition']+' ').repeat(options['additionRepeatTimes']).split(' ').reverse().splice(1).join(`${options['additionSeparator']}`)
+  //return (str+addition_str+' ').repeat(options['repeatTimes']).split(' ').reverse().splice(1).join(`${options['separator']}`)
+  str=str+'';
+  let count = +options.repeatTimes;
+  let custseparator = options.separator;
+  let addition = options.addition;
+  let additionRepeatTimes = +options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator;
+
+  if (typeof addition !== 'undefined') {   
+    let new_str = str + addition;
+    if (typeof additionRepeatTimes !== 'undefined') {
+
+      for (let i = 0; i < additionRepeatTimes - 1; i++) {
+
+        if (typeof additionSeparator !== 'undefined') {
+          new_str += (additionSeparator + addition);
+        }
+        else {
+          new_str = new_str + '|' + addition;
+        }
+      }
+      str=new_str;
+    }
+    else str=str+addition;
+  }
+
+  let final_str = str;
+  for (let i = 0; i < count - 1; i++) {
+    if (typeof custseparator !== 'undefined') {
+      final_str += (custseparator + str);
+    }
+    else {
+      final_str += ('+' + str);
+    }
+  }
+  return final_str;
 };
   
