11781427cacbd4e9c3304929776d434a85b7f047
Nikolas <erosenkov2017@gmail.com>
2021-03-17 22:03:38 +0300

    feat: add solituon of max_depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let max_depth = 0;
+    max_depth = Math.max(...arr.map(el => Array.isArray(el) ? this.calculateDepth(el) : 0), max_depth)
+    return max_depth + 1;
   }
 };
