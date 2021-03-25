29bfbccbc2ebb97b92dc5f247bb613d727d56cae
Nikolas <erosenkov2017@gmail.com>
2021-03-25 14:17:06 +0300

    feat: add solution for lasts tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,89 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    this.direction = direction === false ? "reverse" : "direct";
+  }
+
+  encrypt(input, key) {
+    if (!input || !key || input.length <= 0 || key.length <= 0) {
+      throw new Error();
+    }
+    
+    input = input.toUpperCase();
+    key = key.toUpperCase();
+
+    let res = [];
+
+    for (let i = 0, j = 0; i < input.length; i++) {
+      if (!input[i].match(/[A-Z]/)) {
+        res.push(input[i]);
+        continue;
+      }
+
+      let inputCharCode = input[i].charCodeAt(0);
+      let keyCharCode = key[j].charCodeAt(0);
+      j++;
+
+      if (j >= key.length) {
+        j = 0;
+      }
+
+      let encrypted = String.fromCharCode(
+        ((inputCharCode + keyCharCode) % 26) + 65
+      );
+
+      res.push(encrypted);
+    }
+
+    if (this.direction === "reverse") {
+      res.reverse();
+    }
+
+    return res.join("");
+  }
+
+  decrypt(encryptedWord, key) {
+    if (
+      !encryptedWord ||
+      !key ||
+      encryptedWord.length <= 0 ||
+      key.length <= 0
+    ) {
+      throw new Error();
+    }
+
+    encryptedWord = encryptedWord.toUpperCase();
+    key = key.toUpperCase();
+
+    let res = [];
+
+    for (let i = 0, j = 0; i < encryptedWord.length; i++) {
+      if (!encryptedWord[i].match(/[A-Z]/)) {
+        res.push(encryptedWord[i]);
+        continue;
+      }
+
+      let encryptedCharCode = encryptedWord[i].charCodeAt();
+      let keyCharCode = key[j].charCodeAt();
+      j++;
+
+      if (j >= key.length) {
+        j = 0;
+      }
+
+      let decrypted = String.fromCharCode(
+        ((encryptedCharCode + 26 - keyCharCode) % 26) + 65
+      );
+
+      res.push(decrypted);
+    }
+
+    if (this.direction === "reverse") {
+      res.reverse();
+    }
+
+    return res.join("");
   }
 }
 
